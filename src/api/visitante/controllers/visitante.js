464a0c46352770cb4c8e05d038a09b91249d5c30
Bryan Sepia <bryanromero@gmail.com>
2022-11-18 17:33:14 -0600

    update version strapi

---
+++ src/api/visitante/controllers/visitante.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * visitante controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::visitante.visitante');
---
+++ src/api/visitante/routes/visitante.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * visitante router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::visitante.visitante');
